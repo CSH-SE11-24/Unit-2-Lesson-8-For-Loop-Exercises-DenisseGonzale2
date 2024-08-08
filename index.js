1f65df2993633eb290abd5428244f44854739a20
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 18:46:07 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// Task 1: Print Numbers
+// Write a JavaScript program that uses a for loop to print all numbers from 1 to 10 to the console.
+
+let i= 1
+while(i<11){
+	console.log(i)
+	i++
+}
+// Task 2: Odd Numbers
+// Write a JavaScript program that uses a for loop to print all odd numbers from 15 to 40 to the console.
+
+let num = 15
+while(num<40){
+	console.log(num)
+	num += 2 
+}
+
+// Task 3: Sum of Numbers
+// Write a JavaScript program that calculates and prints the sum of all numbers from 10 to 20 using a for loop.
+
+for(let a = 10; a <= 20; a++ ){
+	console.log("The sum is", a)
+}
+
+// Task 4: Array Iteration
+// Create an array of numbers (e.g., [1, 2, 3, 4, 5]) and write a JavaScript program that uses a for loop to iterate through the array and print each element to the console.
+
+let arrayNum = [1, 2 , 3, 4, 5 ]
+
+for(let random = 0; random < arrayNum.length; random++){
+	console.log(random)
+}
+
+// Task 5: Array Sum
+// Create an array of numbers and write a JavaScript program that calculates and prints the sum of all the elements in the array using a for loop.
+
+let numbers =[1, 2, 3, 4]
+
+for (let i = 0;i< numbers.length;i++){
+	console.log(numbers[i])
+}
+
+
+// Stretch: Element counter
+// Create an array of numbers
+// Prompt the user for a number and write a for loop that tracks how many times that number is in the array
+// e.g. 5 for [5, 1, 2, 5] --> 2
+
+
+
+
+
